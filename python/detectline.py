7c1df0b0bfc22666e24ab4bde06fb494afaf371e
Kevin Tain <ktain@ucsd.edu>
2017-10-23 02:00:12 -0700

    Implemented nearest peak detection in python

--- python/detectline.py
+++ python/detectline.py
@@ -52,6 +52,32 @@ def get_max_index(arr):
             max_val = arr[i]
     return max_index
 
+def get_nearest_peak(arr, threshold):
+    min_width = 5
+    
+    lhs = 0
+    rhs = 0
+    line_pos = 0
+    center = len(arr)/2
+    min_dist_from_center = int(len(arr))/2 - 1
+    peak_count = 0
+    
+    for i in range(int(len(arr))):
+        if arr[i] >= threshold and i > rhs:
+            lhs = i
+            for i in range(lhs, (int(len(arr)))):
+                if (arr[i] < threshold) or (i >= int(len(arr))-1):
+                    rhs = i
+                    peak_count = peak_count + 1
+                    break
+            if (rhs - lhs >= min_width) and abs(lhs + (rhs - lhs)/2.0 - center) < abs(min_dist_from_center):
+                line_pos = lhs + (rhs - lhs)/2.0
+                min_dist_from_center = abs(lhs + (rhs - lhs)/2.0 - center)
+            
+    print '%d peaks detected'%(peak_count)
+    return line_pos
+        
+
 # open the COM5 port at 921600 baud
 ser = serial.Serial('COM5', 921600)
 count = 0;
@@ -73,24 +99,26 @@ try:
         # Refresh plot
         if count >= 128:
             
-            # Calculate threshold
-            threshold = min(array128) + (max(array128) - min(array128))/2.0
-            
             # Median filter
-            array128 = median_filter(array128)
+            # array128 = median_filter(array128)
             
             # Mean filter
             array128 = mean_filter(array128)
-            array128 = mean_filter(array128)
                 
+            # Calculate threshold
+            threshold = min(array128) + 0.3*(max(array128) - min(array128))
+            
             # Update line position
             max_index = get_max_index(array128)
-            line_pos = line_pos + 0.5*(max_index-line_pos)
+            
+            # Detect nearest peak
+            nearest_peak = get_nearest_peak(array128, threshold)
 
             # Plot
             plt.plot(array128, linewidth=2)
             plt.ylim(0, 1000)
             plt.xlim(0, 127)
+            plt.grid(True)
             
             # Plot threshold
             plt.axhline(y=threshold, linewidth=2, alpha=0.5, color='red')
@@ -99,10 +127,11 @@ try:
             plt.axvline(x=63.5, linewidth=1, ls='dashed', alpha=0.2, color='grey')
             
             # Plot max index
-            plt.axvline(x=max_index, linewidth=2, alpha=0.5, color='purple')
+            plt.axvline(x=max_index, linewidth=15, alpha=0.5, color='green')
             
-            # Plot line position
-            plt.axvline(x=line_pos, linewidth=10, alpha=0.2, color='green')
+            # Plot first peak
+            plt.axvline(x=nearest_peak, linewidth=15, alpha=0.5, color='brown')
+            
             
             plt.pause(0.040)  # pause in seconds
             count = 0
